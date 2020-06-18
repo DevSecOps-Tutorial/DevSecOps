a61e0cfd89959464dbcb88d808c1100e3734c04c
Prakash Tomar <prakash_tomar@outlook.com>
2020-06-18 20:13:14 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://13.235.50.143:9000',
+       serverUrl: 'http://13.127.182.144:9000',
        options : {
        'sonar.sources': '.',
        //'sonar.inclusions' : '.' // Entry point of your code
